cb296220714c72962bba159d20adc2b52a8ddcb3
Tony <antonipantk@gmail.com>
2026-03-28 14:27:41 +0000

    Main Window + Git Test (Actual Code)

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
-print("Hello World")
+#To push changes to github:
+#   git add . (Selects all files)
+#   git commit -m "describe what you changed" (Keep the quotation)
+#   git push (Updates all files)
+#
+#If you are switching devices:
+#   git pull (Applies most recent changes to the current device)
+import pygame
+import sys
+
+pygame.init()
+size = (1280,720)
+screen = pygame.display.set_mode(size)
+clock = pygame.time.Clock()
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
